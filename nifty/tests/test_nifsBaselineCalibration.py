fa12c2867fea17bcee15f59e1723e223e7d9c0e0
Nat1405 <nathanc1945@gmail.com>
2020-07-07 08:52:38 -0700

    Adds short nifsBaselineCalibration int test.
    This also checks that datalabels and prefixes only contain 'g' or 'G' if the frame is the result of a co-add with n>1 frames in gemcombine.

--- nifty/tests/test_nifsBaselineCalibration.py
+++ nifty/tests/test_nifsBaselineCalibration.py
@@ -38,6 +38,104 @@ def download_test_data(path):
     for frame in glob.glob(os.path.join(data_path("GN-2014A-Q-85-all"), "N20140428*")):
         shutil.copy(frame, data_path("GN-2014A-Q-85_one_day"))
 
+
+def test_nifsBaselineCalibrationQuick(tmpdir, monkeypatch):
+    """
+        - Make shift image
+
+        - Make NIFS flat field image from 1 input flats, 1 flat dark,
+        and check metadata looks okay.
+
+        - Make processed Arc from 1 lamps-on arc and 1 lamps-off arc
+
+        - Make processed Ronchi from 1 lamps-on ronchi
+    """
+
+    tmpdir = str(tmpdir)
+    os.mkdir(os.path.join(tmpdir, 'K'))
+    os.mkdir(os.path.join(tmpdir, 'Calibrations_K'))
+    
+    os.chdir(os.path.join(tmpdir, 'Calibrations_K'))
+    
+    def mocklog(foo):
+        pass
+    monkeypatch.setattr(logging, "info", mocklog)
+    monkeypatch.setattr(logging, "warning", mocklog)
+    monkeypatch.setattr(logging, "error", mocklog)
+
+    # Copy config file over and set it up for testing.
+    RECIPES_PATH = pkg_resources.resource_filename('nifty', 'recipes/')
+    shutil.copy(os.path.join(RECIPES_PATH, 'defaultConfig.cfg'), os.path.join(os.getcwd(), 'config.cfg'))
+
+    if not os.path.exists(data_path('GN-2014A-Q-85-all')):
+        download_test_data(data_path('GN-2014A-Q-85-all'))
+
+    with open('config.cfg') as config_file:
+        config = ConfigObj(config_file, unrepr=True)
+    
+    config['ManualMode'] = False
+    config['over'] = False
+    config['scienceDirectoryList'] = []
+    config['telluricDirectoryList'] = []
+    config['calibrationDirectoryList'] = [os.getcwd()]
+
+    with open('config.cfg', 'w') as config_file:
+        config.write(config_file)
+
+    # Copy relevant calibrations over, and write lists.
+    flatlist = [
+        'N20140428S0169.fits'
+    ]
+    
+    flatdarklist = [
+        'N20140428S0174.fits'
+    ]
+
+    arclist = [
+        'N20140428S0085.fits'
+    ]
+
+    arcdarklist = [
+        'N20140428S0179.fits'
+    ]
+
+    ronchilist = [
+        'N20140428S0181.fits'
+    ]
+
+    for frame in flatlist:
+        shutil.copy(data_path(os.path.join('GN-2014A-Q-85-all', frame)), os.getcwd())
+    for frame in flatdarklist:
+        shutil.copy(data_path(os.path.join('GN-2014A-Q-85-all', frame)), os.getcwd())
+    for frame in arclist:
+        shutil.copy(data_path(os.path.join('GN-2014A-Q-85-all', frame)), os.getcwd())
+    for frame in arcdarklist:
+        shutil.copy(data_path(os.path.join('GN-2014A-Q-85-all', frame)), os.getcwd())
+    for frame in ronchilist:
+        shutil.copy(data_path(os.path.join('GN-2014A-Q-85-all', frame)), os.getcwd())
+
+    with open('flatlist', 'w') as f:
+        f.write('\n'.join([x.split('.')[0] for x in flatlist]))
+    with open('flatdarklist', 'w') as f:
+        f.write('\n'.join([x.split('.')[0] for x in flatdarklist]))
+    with open('arclist', 'w') as f:
+        f.write('\n'.join([x.split('.')[0] for x in arclist]))
+    with open('arcdarklist', 'w') as f:
+        f.write('\n'.join([x.split('.')[0] for x in arcdarklist]))
+    with open('ronchilist', 'w') as f:
+        f.write('\n'.join([x.split('.')[0] for x in ronchilist]))
+
+    nifsBaselineCalibration.start()
+
+    with fits.open(os.path.join(tmpdir, 'Calibrations_K', 'rnN20140428S0169_flat.fits')) as hdul:
+        assert hdul['PRIMARY'].header['DATALAB'] == 'GN-2014A-Q-85-16-001-RN-FLAT'
+
+    with fits.open(os.path.join(tmpdir, 'Calibrations_K', 'rnN20140428S0181_ronchi.fits')) as hdul:
+        assert hdul['PRIMARY'].header['DATALAB'] == 'GN-2014A-Q-85-16-013-RN-RONCHI'
+
+    with fits.open(os.path.join(tmpdir, 'Calibrations_K', 'wrnN20140428S0085_arc.fits')) as hdul:
+        assert hdul['PRIMARY'].header['DATALAB'] == 'GN-2014A-Q-85-12-001-WRN-ARC'
+
    
 def test_nifsBaselineCalibration(tmpdir, monkeypatch):
     """
@@ -145,9 +243,6 @@ def test_nifsBaselineCalibration(tmpdir, monkeypatch):
 
     with fits.open(os.path.join(tmpdir, 'Calibrations_K', 'wrgnN20140428S0085_arc.fits')) as hdul:
         assert hdul['PRIMARY'].header['DATALAB'] == 'GN-2014A-Q-85-12-001-WRGN-ARC'
-
-
-
 
 
 
